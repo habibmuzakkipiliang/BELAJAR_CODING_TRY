b52b9dd1862f31a8d7818a7face6e2f0c9ddcd5b
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-04-17 20:22:25 +0700

    Add files via upload

---
+++ 1._Belajar_TypeScript_(JS_Pro)/1._Testing_TypeScript.ts
@@ -0,0 +1,61 @@
+// Tes Program TypeScript Sederhana
+
+
+console.log ("Hello World by TypeScript \n")
+
+
+var selamat : string = "Selamat kembali Habib, di Windows 11 Pro yang sudah diperbaiki mandiri di rumah selama bulan puasa 2026 (HP Elitebook 8440p, RAM 8 GB, SSD Samsung Ovo 496 GB)\n"
+
+console.log (selamat)
+
+
+
+
+var tes : string = "Semangat, untuk ujian akhir sekolah dan belajar IT nya \n"
+
+console.log (tes)
+
+
+
+
+var tes_1 : string = "Semangat belajar Coding Python dan JavaScript (HTML dan CSS) \n"
+
+console.log (tes_1)
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("FANS (WOTA) IDOL & OSHI (MEMBER FAVORIT) \n")
+
+
+var idol : string = "Grup Idol Favorit: JKT48 | AKB48 | Rain Tree \n"
+
+console.log (idol)
+
+
+var oshi = [
+    
+    "1. Michie JKT48 (Utama)",
+    "2. Fritzy JKT48 (Utama)",
+    "3. Anindya JKT48 (Utama)",
+    "4. Christy JKT48 (Utama)",
+    "5. Freya JKT48",
+    "6. Olla JKT48", 
+    "7. Jessi JKT48",
+    "8. Fiony JKT48",
+    "9. Muthe JKT48",
+    "10. Marsha JKT48",
+    "11. Eli JKT48",
+    "12. Mikaela JKT48",
+    "13. Yui Oguri (AKB48)",
+    "14. Endo Rino (Rain Tree)",
+    
+    ]
+    
+    
+for (var a = 0; a < oshi.length; a++) {
+    console.log (oshi [a])
+}
---
+++ 1._Belajar_TypeScript_(JS_Pro)/Hello_Awal_TypeScript.ts
@@ -0,0 +1,566 @@
+// Belajar Awal TypeScript
+
+
+console.log ("\n Belajar Awal TypeScript \n")
+
+ 
+console.log ("Hello World")
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+// Tes Awal TypeScript
+
+console.log ("\n Tes Awal TypeScript \n")
+
+
+console.log ("Hello JKT48")
+console.log ("Hello Kapten JKT48")
+console.log ("Hello Jansen")
+console.log ("Hello anak JKT48")
+console.log ("Hello anak gen 13 JKT48")
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+// Sintaks dasar TypeScript
+
+
+console.log ("\n Sintaks dasar TypeScript \n")
+
+
+let a : string = "Tes"
+
+console.log ("Teks =", a)
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+let b : number = 23
+
+console.log ("Angka =", b)
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+let c : number = 34.23
+
+console.log ("Desimal =", c)
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+let d : string = 'C'
+
+console.log ("Char =", d)
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+let e = false 
+
+console.log ("Cek_1 =", e)
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+let f = true
+
+console.log ("Cek_2 =", f)
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+// Bedanya Let, Var, Const 
+
+
+console.log ("\n Bedanya Let, Var, Const \n")
+
+
+console.log ("\n Pake Let \n")
+
+
+let teks_s : string = "Halo Anak JKT48"
+let angka_s : number = 34
+let desimal_s : number = 34.23
+let char_s : string = 'A'
+let cek_s = true
+let cek_s2 = false 
+
+
+console.log ("Teks s =", teks_s)
+console.log ("Angka s =", angka_s)
+console.log ("Desimal s =", desimal_s)
+console.log ("Char s =", char_s)
+console.log ("cek s =", cek_s)
+console.log ("cek_s2 =", cek_s2)
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+console.log ("\n Pake Var \n")
+
+
+var teks_d : string = "Halo anak gen 11 JKT48"
+var angka_d : number = 23
+var desimal_d : number = 33.24
+var char_d : string = 'A'
+var cek_d = true
+var cek_d2 = false 
+
+
+console.log ("Teks d =", teks_d)
+console.log ("Angka d =", angka_d)
+console.log ("Desimal d =", desimal_d)
+console.log ("Char d =", char_d)
+console.log ("Cek d =", cek_d)
+console.log ("cek d2 =", cek_d2)
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Pake Const \n")
+
+
+const teks_e : string = "Halo anak gen 12 JKT48"
+const angka_e : number = 23
+const desimal_e : number = 34.23
+const char_e : string = 'A'
+const cek_e = false
+const cek_e2 = true
+
+
+console.log ("Teks e =", teks_e)
+console.log ("Angka e =", angka_e)
+console.log ("Desimal e =", desimal_e)
+console.log ("Char e =", char_e)
+console.log ("Cek e =", cek_e)
+console.log ("cek e2 =", cek_e2)
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+// Tipe data dasar 
+
+
+console.log ("\n Tipe data dasar \n")
+
+
+var teks : string = "Halo Jakarta"
+var angka : number = 23
+var desimal : number = 34.23
+var cek_a = false
+var cek_b = true
+var char : string = 'A'
+var daftar : string[] = ["Michie JKT48", "Olla JKT48", "Fritzy JKT48", "Eli JKT48"]
+
+
+console.log ("Teks =", teks)
+console.log ("Angka =", angka)
+console.log ("Cek A =", cek_a)
+console.log ("Cek B =", cek_b)
+console.log ("Char =", char)
+console.log ("Daftar =", daftar)
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+// Operator dasar 
+
+console.log ("\n Operator dasar \n")
+
+
+let x : number = 10
+let y : number = 5
+
+
+console.log ("Tambah =", x + y)
+console.log ("Kurang =", x - y)
+console.log ("Kali =", x * y)
+console.log ("Pangkat =", x ** y)
+console.log ("Bagi =", x / y)
+console.log ("Modulus =", x % y)
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+console.log ("\n Operator Perbandingan \n")
+
+
+console.log ("Hasil =", x > y)
+console.log ("Hasil =", x < y)
+console.log ("Hasil =", x <= y)
+console.log ("Hasil =", x >= y)
+console.log ("Hasil =", x == y)
+console.log ("Hasil =", x != y)
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+// Operator Logika
+
+console.log ("\n Operator Logika \n")
+
+
+console.log ("Hasil =", (x > y) && (x < y))
+console.log ("Hasil =", (x < y) || (x > y))
+console.log ("Hasil =", (!x))
+console.log ("Hasil =", (!y))
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+// Profil lengkap Habib Muzakki
+
+console.log ("\n Profil lengkap Habib Muzakki \n")
+
+
+var nama : string = "Habib Muzakki"
+var panggilan : string  = "Habib"
+var marga : string  = "Piliang"
+var gen : string  = "Gen Z"
+var lahir : string  = "Bukittinggi, 22 Februari 2007"
+var kelas : string  = "12 Agama"
+var absen : number = 15
+var sekolah : string  = "MAN 2 KOTA SERANG"
+var asal : string  = "Kota Bukittinggi"
+var tinggal : string  = "Kota Serang"
+var tinggi : string = "172 cm"
+var berat : string  = "60 kg"
+var gol : string  = "B"
+var lomba : string  = "OSN-K Informatika (hanya sampai tingkat kota saja)"
+var coding : string  = "HTML, CSS, JavaScript dan Python"
+var passion : string  = "Teknologi, Digital dan IPS"
+var hobi : string  = "Ngoding, JKT48, Rain Tree (Non Grup 48), AKB48, Nonton Creepypasta Minecraft Jundy Juns dan Main Game Offline"
+var fans : string  = "JKT48, AKB48 dan Rain Tree"
+var oshi : string  = "Fritzy, Olla, Christy, Marsha, Freya, Jessi, Muthe, Eli, Fiony, Mikaela, Michie, Yui Oguri (AKB48) dan Endo Rino (Rain Tree)"
+
+
+console.log ("Nama lengkap :", nama)
+
+console.log ("Nama panggilan :", panggilan)
+
+console.log ("Marga :", marga)
+
+console.log ("Gen :", gen)
+
+console.log ("Lahir :", lahir)
+
+console.log ("Absen :", absen)
+
+console.log ("Sekolah :", sekolah)
+
+console.log ("Asal :", asal)
+
+console.log ("Tempat Tinggal :", tinggal)
+
+console.log ("Tinggi badan :", tinggi)
+
+console.log ("Berat badan :", berat)
+
+console.log ("Golongan darah :", gol)
+
+console.log ("Lomba :", lomba)
+
+console.log ("Coding yang sedang dipelajari :", coding)
+
+console.log ("Passion :", passion)
+
+console.log ("Hobi :", hobi)
+
+console.log ("Fans :", fans)
+
+console.log ("Oshi :", oshi)
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+// Manipulasi String
+
+
+console.log ("\n Manipulasi String \n")
+
+
+console.log (`Halo saya ${nama}, nama panggilannya ${panggilan}, marga saya adalah ${marga}, saya termasuk gen ${gen}, lahir di ${lahir}, dari kelas ${kelas}, absen ke - ${absen}, dari sekolah ${sekolah}, berasal dari ${asal}, tempat tinggal nya di ${tinggal}, tinggi badan saya adalah ${tinggi}, berat badan saya adalah ${berat}, golongan darahnya ${gol}, saya sudah mengikuti lomba ${lomba}, dan bahasa pemrograman yang saya pelajari adalah ${coding}, passion saya adalah ${passion}, hobi saya adalah ${hobi}, saya ngefans dengan ${fans}, dan oshi saya adalah ${oshi}`)
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+// Percabangan dasar
+
+console.log ("\n Percabangan dasar \n")
+
+let ak : number = 8
+
+
+if (ak > 5) {
+    console.log ("Besar")
+}
+
+else {
+    console.log ("Kecil")
+}
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+// Percabangan dasar 2
+
+console.log ("\n Percabangan dasar 2 \n")
+
+let bk : number = 3
+
+if (bk > 5) {
+    console.log ("Besar")
+}
+
+else {
+    console.log ("Kecil")
+}
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+// Percabangan lanjutan 1
+
+console.log ("\n Percabangan Lanjutan \n")
+
+
+let ck : number = 9
+
+if (ck > 5) {
+    console.log ("Besar")
+}
+
+else if (ck < 5) {
+    console.log ("Kecil")
+}
+
+else {
+    console.log ("Gak ada")
+}
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+// Percabangan Tangga Lanjutan
+
+console.log ("\n Percabangan Tangga Lanjutan\n")
+
+
+let dk : number = 3
+
+
+if (dk > 5) {
+    console.log ("Besar")
+}
+
+else if (dk == 4) {
+    console.log ("4")
+}
+
+else if (dk == 3) {
+    console.log ("3")
+}
+
+else if (dk == 2) {
+    console.log ("2")
+}
+
+else if (dk == 1) {
+    console.log ("1")
+}
+
+else {
+    console.log ("Gak ada")
+}
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+// Percabangan Nested 1
+
+console.log ("\n Percabangan Nested 1 \n")
+
+
+let ek : number = 7
+let cek_x = true
+
+
+if (cek_x) {
+    if (ek > 5) {
+        console.log ("Besar")
+    }
+    
+    else if (ek < 5) {
+        console.log ("Kecil")
+    }
+}
+
+else {
+    console.log ("Semula")
+}
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+// Percabangan Nested 2
+
+console.log ("\n Percabangan Nested 2 \n")
+
+
+let fk : number = 3
+let cek_u = true
+
+
+if (cek_u) {
+    if (fk > 5) {
+        console.log ("Besar")
+    }
+    
+    else {
+        console.log ("Kecil")
+    }
+}
+
+else {
+    console.log ("Semula")
+}
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+// For dasar 
+
+console.log ("\n For dasar \n ")
+
+
+for (let ah = 0; ah < 15; ah++) {
+    console.log ("ah =", ah)
+}
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+// For Nested 
+
+
+console.log ("\n For Nested \n")
+
+
+for (let rt = 0; rt < 8; rt++) {
+    for (let er = 0; er < 8; er++) {
+        console.log ("rt =", rt, "er =", er)
+    }
+}
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+// While dasar 
+
+console.log ("\n While dasar \n")
+
+
+let hj : number = 5
+
+while (hj < 15) {
+    console.log ("hj =", hj)
+    hj++
+}
+
+
+console.log ("\n --- Batas -- \n")
+
+
+
+
+// Do While dasar
+
+console.log ("\n Do While dasar \n")
+
+
+let df : number = 3
+
+do {
+    console.log ("df =", df)
+    df++
+} while (df < 15)
+
+
+console.log ("\n --- Batas -- \n")
