33d0d1c8eed44b93d3800d5d08f27ab433421d3f
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-04-17 20:25:03 +0700

    Add files via upload

---
+++ A._Jadwal_dan_Daftar/Jadwal_Mapel_Kelas_12_Agama_2026_Semester_2.py
@@ -0,0 +1,50 @@
+print("""
+
+JADWAL MAPEL KELAS 12 AGAMA, SEMESTER 2, TAHUN 2026, MAN 2 KOTA SERANG 
+
+
+- Senin: 
+
+1. Bahasa Arab Minat, 
+2. Ekonomi, 
+3. Ushul Fiqih, 
+4. Qurdis, 
+
+
+- Selasa: 
+
+1. Matematika, 
+2. Fiqih, 
+3. Sejarah Indonesia, 
+4. Ilmu Hadis, 
+5. B. Indonesia, 
+
+
+- Rabu:
+
+1. Matematika, 
+2. Qurdis, 
+3. Seni Budaya, 
+4. Ilmu Tafsir, 
+5. Ilmu Kalam,
+
+
+- Kamis: 
+
+1. Prakarya, 
+2. Ekonomi, 
+3. SKI, 
+4. B. Arab Wajib, 
+5. PPkn, 
+
+
+
+- Jumat: 
+
+1. B. Indonesia, 
+2. PJOK, 
+3. B. Inggris, 
+4. BK, 
+5. Akhlak Tasawuf,
+
+""")
---
+++ A._Kartu_Ucapan_Hari_Raya/JavaScript/Kartu_Ucapan_Idul_Fitri_tahun_2026.js
@@ -0,0 +1,36 @@
+// Program Kartu Ucapan Spesial Idul Fitri oleh Coding JavaScript
+
+console.log ("Program Kartu Ucapan Spesial Idul Fitri oleh Coding JavaScript \n")
+
+
+var raya = "Hari Raya Idul Fitri"
+var hijriah = "1447 Hijriah"
+var masehi = "2026 Masehi"
+var nama = "Habib Muzakki"
+var marga = "Piliang"
+var panggil = "Habib"
+var suku = "Minangkabau"
+var kelas = "12 Agama"
+var sekolah = "MAN 2 KOTA SERANG"
+var asal = "Kota Serang, Provinsi Banten"
+var fans = "JKT48"
+var oshi = "Michie, Fritzy, Anindya, Christy JKT48"
+
+
+var ucapan = `Selamat ${raya} ${hijriah}, dan tahun ${masehi}, minal aidin wal faizin, mohon maaf lahir dan batin,  
+
+Salam dari : 
+- Nama    : ${nama} 
+- Marga   : ${marga}
+- Panggil : ${panggil}
+- Suku    : ${suku}
+- Kelas   : ${kelas}
+- Sekolah : ${sekolah}
+- Asal    : ${asal} 
+- Fans    : ${fans} 
+- Oshi    : ${oshi}
+
+`
+
+
+console.log (ucapan)
