53c30e33391a9a19fe8917f23502f50b5b8ce47e
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-04-17 20:27:23 +0700

    Add files via upload

---
+++ Belajar_C++_(Demo)/Tes_C++_Awal.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+	// Hello World by Coding C++
+	
+	cout << "Hello World" << endl;
+	
+	
+	cout << "\n ---- Batas ------ \n" << endl;
+	
+	
+	// Sintaks dasar Coding C++
+	
+	string nama = "Habib Muzakki";
+	int angka = 34;
+	float desimal = 45.33;
+	char huruf = 'A';
+	bool cek = true;
+	
+	cout << "Nama : " << nama << endl;
+	cout << "Angka : " << angka << endl;
+	cout << "Desimal : " << desimal << endl;
+	cout << "Char : " << huruf << endl;
+	cout << "Cek : " << cek << endl;
+	
+	
+	cout << "\n ----- Batas ------ \n" << endl;
+	
+	
+	
+	
+	// Sintaks dasar 1
+	
+	string nama_1 = "Habib Muzakki";
+	int angka_1 = 23;
+	float desimal_1 = 34.45;
+	char huruf_1 = 'A';
+	bool cek_a = true;
+	bool cek_b = false;
+	
+	
+	cout << "nama_1 = " << nama_1 << endl;
+	cout << "angka_1 = " << desimal_1 << endl;
+	cout << "huruf_1 = " << huruf_1 << endl;
+	cout << "Cek a = " << cek_a << endl;
+	cout << "Cek b = " << cek_b << endl;
+	
+	
+	cout << "\n ---- Batas ------ \n" << endl;
+	
+	
+	
+	
+	// Profil Habib Muzakki
+	
+	string nama_2 = "Habib Muzakki Piliang";
+	string kelas_2 = "12 Agama";
+	string sekolah_2 = "MAN 2 KOTA SERANG";
+	string akrab_2 = "Habib";
+	int absen_2 = 15;
+	float tinggi_2 = 170.3;
+	char huruf_2 = 'A';
+	bool cek_2 = true;
+	
+	
+	cout << "nama = " << nama_2 << endl;
+	cout << "kelas = " << kelas_2 << endl;
+	cout << "sekolah = " << sekolah_2 << endl;
+	cout << "akrab = " << akrab_2 << endl;
+	cout << "absen = " << absen_2 << endl;
+	cout << "tinggi = " << tinggi_2 << endl;
+	cout << "huruf = " << huruf_2 << endl;
+	cout << "cek = " << cek_2 << endl;
+	
+	
+	cout << "\n ---- Batas ------ \n" << endl;
+	
+	
+	
+	
+	// Percabangan dasar
+	
+	int an = 7;
+	
+	if (an > 5) {
+	    cout << "Besar" << endl;
+	}
+	
+	else {
+	    cout << "Kecil" << endl;
+	}
+	
+	
+	cout << "\n ---- Batas ------ \n" << endl;
+	
+	
+	
+	
+	// Percabangan dasar 2
+	
+	int ak = 3;
+	
+	if (ak > 5) {
+	    cout << "Besar" << endl;
+	}
+	
+	else {
+	    cout << "Kecil" << endl;
+	}
+	
+	
+	return 0;
+}
