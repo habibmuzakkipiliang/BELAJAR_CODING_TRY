541f583b1e3bfd6469d104c8384f541f67e8c36e
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-04-20 16:26:24 +0700

    Add files via upload

---
+++ 1._A._Latihan_Random_Coding/Random_Python/Random_Python.py
@@ -0,0 +1,55 @@
+x = 5
+y = x 
+y = 10
+
+print (y)
+
+
+print ("\n --- \n")
+
+
+for i in range (3):
+    print (i)
+    
+    
+print ("\n --- \n")
+
+    
+    
+    
+def halo ():
+    print ("Hai")
+    
+halo ()
+
+
+print ("\n --- \n")
+
+
+
+
+angka = [1, 2, 3]
+angka.append (4)
+print (len (angka))
+
+
+print ("\n --- \n")
+
+
+
+if True and False:
+    print ("A")
+    
+else:
+    print ("B")
+    
+    
+print ("\n --- \n")
+
+    
+    
+    
+x = 15
+y = 4
+
+print (x % y)
