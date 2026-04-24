2a5eb89983b900876d8e2076693fd3f707534278
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-04-24 08:10:16 +0700

    Add files via upload

---
+++ 1._A._Latihan_Random_Coding/Random_Python/Python_Task.py
@@ -0,0 +1,72 @@
+# JavaScript Task
+
+print ("Hello World")
+
+
+print ("\n --- Batas --- \n")
+
+
+
+print ("\n Variabel dan Tipe data \n")
+
+
+nama = "Tes 1"
+angka = 34
+desimal = 44.23
+huruf = 'A'
+cek = True
+daftar = [
+    
+    "1. Mobil",
+    "2. Motor", 
+    "3. Kereta Api",
+    "4. Bajai",
+    
+    ]
+
+
+detail = f"""
+
+- Nama    : {nama}
+- Angka   : {angka}
+- Desimal : {desimal}
+- Huruf   : {huruf}
+- Cek     : {cek}
+- Daftar  : 
+
+"""
+
+print (detail)
+
+
+for a in daftar:
+    print (a)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Dictionary \n")
+
+
+data = {
+    "nama" : "Alan John",
+    "asal" : "Amerika Serikat",
+    "kerja" : "Software Engineer",
+    "coding" : "HTML, CSS, JavaScript dan Python",
+    
+}
+
+
+print ("Nama :", data ["nama"])
+
+print ("Asal :", data ["asal"])
+
+print ("Kerja :", data ["kerja"])
+
+print ("Coding :", data ["coding"])
+
+
+print ("\n --- Batas --- \n")
