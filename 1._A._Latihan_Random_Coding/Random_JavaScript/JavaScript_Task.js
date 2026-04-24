6ba4c3e2cf2707039c6fff1ec7a65169a133bac3
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-04-24 08:12:38 +0700

    Add files via upload

---
+++ 1._A._Latihan_Random_Coding/Random_JavaScript/JavaScript_Task.js
@@ -0,0 +1,75 @@
+// JavaScript Task
+
+console.log ("Hello World")
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+console.log ("\n Variabel dan Tipe data \n")
+
+
+var nama = "Tes 1"
+var angka = 34
+var desimal = 44.23
+var huruf = 'A'
+var cek = true
+var daftar = [
+    
+    "1. Mobil",
+    "2. Motor", 
+    "3. Kereta Api",
+    "4. Bajai",
+    
+    ]
+
+
+detail = `
+
+- Nama    : ${nama}
+- Angka   : ${angka}
+- Desimal : ${desimal}
+- Huruf   : ${huruf}
+- Cek     : ${cek}
+- Daftar  : 
+
+`
+
+console.log (detail)
+
+
+for (a = 0; a < daftar.length; a++) {
+    console.log (daftar [a])
+}
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Dictionary \n")
+
+
+var data = {
+    "nama" : "Alan John",
+    "asal" : "Amerika Serikat",
+    "kerja" : "Software Engineer",
+    "coding" : "HTML, CSS, JavaScript dan Python",
+    
+}
+
+
+console.log ("Nama :", data ["nama"])
+
+console.log ("Asal :", data ["asal"])
+
+console.log ("Kerja :", data ["kerja"])
+
+console.log ("Coding :", data ["coding"])
+
+
+console.log ("\n --- Batas --- \n")
+
+
