171f24a7f9640ea0d9047f9eaa1ab2c6b4a4b7f6
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-04-24 12:54:42 +0700

    Add files via upload

---
+++ 1._A._Latihan_Random_Coding/Random_Python/1._A._Python_Task.py
@@ -0,0 +1,188 @@
+# Python Task
+
+print ("Hello World")
+
+
+print ("\n --- Batas --- \n")
+
+
+
+print ("\n Variabel dan Tipe data pemrograman \n")
+
+
+nama = "Tes 1"
+angka = 34
+desimal = 44.23
+huruf = 'A'
+cek = True
+daftar = [
+    
+    "1. Mobil",
+    "2. Motor", 
+    "3. Kereta Api",
+    "4. Bajai",
+    
+    ]
+
+
+detail = f"""
+
+- Nama    : {nama}
+- Angka   : {angka}
+- Desimal : {desimal}
+- Huruf   : {huruf}
+- Cek     : {cek}
+- Daftar  : 
+
+"""
+
+print (detail)
+
+
+for a in daftar:
+    print (a)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Dictionary \n")
+
+
+data = {
+    "nama" : "Alan John",
+    "asal" : "Amerika Serikat",
+    "kerja" : "Software Engineer",
+    "coding" : "HTML, CSS, JavaScript dan Python",
+    
+}
+
+
+print ("Nama :", data ["nama"])
+
+print ("Asal :", data ["asal"])
+
+print ("Kerja :", data ["kerja"])
+
+print ("Coding :", data ["coding"])
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Percabangan Dasar \n")
+
+
+a = 9
+
+if a > 5:
+    print (f"Besar, a = {a}")
+    
+else:
+    print (f"Kecil, a = {a}")
+    
+    
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Percabangan dasar 1 \n")
+
+
+b = 3
+
+if b > 5:
+    print (f"Besar, b = {b}")
+    
+else:
+    print (f"Kecil, b = {b}")
+    
+    
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Percabangan Lanjutan 1\n")
+
+
+c = 9
+
+if c > 5:
+    print (f"Besar, c = {c}")
+    
+elif c < 5:
+    print (f"Kecil, c = {c}")
+    
+else:
+    print (f"Sama saja, c = {c}")
+    
+    
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Percabangan Lanjutan 2\n")
+
+
+d = 3
+
+if d > 5:
+    print (f"Besar, d = {d}")
+    
+elif d < 5:
+    print (f"Kecil, d = {d}")
+    
+else:
+    print (f"Sama saja, d = {d}")
+    
+    
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Percabangan Nested 1 \n")
+
+skor = 9
+cek = True
+
+if cek:
+    if skor > 5:
+        print (f"Besar, skor = {skor}")
+        
+    elif skor < 5:
+        print (f"Kecil, skor = {skor}")
+        
+else:
+    print (f"Sama saja, skor = {skor}")
+    
+    
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Percabangan Nested 2 \n")
+
+skor = 3
+cek = True
+
+if cek:
+    if skor > 5:
+        print (f"Besar, skor = {skor}")
+        
+    else:
+        print (f"Kecil, skor = {skor}")
+        
+else:
+    print (f"Sama saja, skor = {skor}")
+
+
+print ("\n --- Batas --- \n")
