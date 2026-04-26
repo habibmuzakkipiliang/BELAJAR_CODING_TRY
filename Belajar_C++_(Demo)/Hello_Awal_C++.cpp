6413f405f73c51604ccad4328bf3c3a284bd401a
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-04-26 17:05:48 +0700

    Add files via upload

---
+++ Belajar_C++_(Demo)/Hello_Awal_C++.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	
+	// Hello World by C++
+	
+	cout << "Hello World" << endl;
+	
+	
+	cout << "\n --- Batas ---- \n" << endl;
+	
+	
+	cout << "\n Tipe data Dasar \n" << endl;
+	
+	string teks = "Halo Michie JKT48";
+	int angka = 12;
+	float desimal = 34.2;
+	bool cek_1 = true;
+	bool cek = false;
+	char huruf = 'A'; 
+	
+	
+	cout << "Teks =" << teks << endl;
+	cout << "Angka =" << angka << endl;
+	cout << "Desimal =" << desimal << endl;
+	cout << "Cek 1 =" << cek_1 << endl;
+	cout << "Cek =" << cek << endl;
+	cout << "Char =" << huruf << endl;
+	
+	
+	cout << "\n --- Batas --- \n" << endl; 
+	
+	return 0;
+}
