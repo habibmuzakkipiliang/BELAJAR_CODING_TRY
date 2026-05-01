7f666f0161d1561c229ad306a556d005448eb6a7
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-05-01 09:09:20 +0700

    Add files via upload

---
+++ 1._A._Latihan_Random_Coding/Coding_Python/Kartu_Ucapan_Jadwal_Ujian_Praktek_Ujian_Akhir_MAN_2_KOTA_SERANG.py
@@ -0,0 +1,44 @@
+# Jadwal Ujian Sekolah dan Ujian Praktek tahun 2026 MAN 2 KOTA SERANG, Angkatan 34 ASCENDRIA, kelas 12 Agama
+
+print ("\n Jadwal Ujian Sekolah dan Ujian Praktek tahun 2026 MAN 2 KOTA SERANG, Angkatan 34 ASCENDRIA, kelas 12 Agama \n")
+
+
+nama = "Habib Muzakki \n"
+marga = "Piliang \n"
+kelas = "12 Agama \n"
+jurusan_sma = "Keagamaan (Anak Madrasah) \n"
+absen = "15 \n"
+sekolah = "MAN 2 KOTA SERANG \n"
+lomba = "Finalis OSN Tingkat Kota Bidang Informatika tahun 2025 \n"
+coding = "HTML, CSS, JavaScript dan Python \n"
+angkatan = "34 ASCENDRIA \n"
+tahun = "2023 - 2026 \n"
+
+
+ucapan = f"""Selamat mengikuti ujian Tulis dan Ujian Praktek ya untuk {nama} dari kelas {kelas}, jurusan {jurusan_sma} \n"""
+
+
+print (ucapan)
+
+
+
+
+print (f"""
+
+--- Profil Habib Muzakki ---
+
+Nama        : {nama}
+Marga       : {marga}
+Kelas       : {kelas}
+Jurusan SMA : {jurusan_sma}
+Absen       : {absen}
+Sekolah     : {sekolah}
+Lomba       : {lomba}
+Coding      : {coding}
+Angkatan    : {angkatan}
+Tahun       : {tahun}
+
+
+--- Batas --- 
+
+""")
