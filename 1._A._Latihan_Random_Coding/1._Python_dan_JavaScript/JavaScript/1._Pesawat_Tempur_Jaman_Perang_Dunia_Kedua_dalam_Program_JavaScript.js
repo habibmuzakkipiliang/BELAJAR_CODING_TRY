36a23c31cd024b731156dbd9817a83605c361aff
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-05-02 16:28:40 +0700

    Add files via upload

---
+++ 1._A._Latihan_Random_Coding/1._Python_dan_JavaScript/JavaScript/1._Pesawat_Tempur_Jaman_Perang_Dunia_Kedua_dalam_Program_JavaScript.js
@@ -0,0 +1,199 @@
+// Pesawat Tempur Jaman Perang dunia kedua dalam Program JavaScript
+
+
+console.log ("\n Pesawat Tempur Jaman Perang Dunia Kedua dalam Program JavaScript \n")
+
+
+function amerika_serikat () {
+    
+    console.log ("\n Amerika Serikat : \n ")
+    
+    var plane = [
+        
+        "1. F4F Wildcat",
+        "2. SBD Dauntless",
+        "3. F4U Corsair",
+        "4. F6F Hellcat",
+        "5. P-51 Mustang",
+        "6. P-38 Lightning",
+        "7. SB2C Helldiver",
+        "8. B-29 Superfortress",
+        "9. B-17 Flying Fortress",
+        "10. A-20 Havoc",
+        "11. B-25 Mitchell",
+        "12. PBY Catalina",
+        
+        
+        ]
+        
+    for (a = 0; a < plane.length; a++) {
+        console.log (plane [a])
+    }
+    
+    
+}
+
+    
+amerika_serikat ()
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+function soviet () {
+    
+    console.log ("\n Uni Soviet : \n")
+    
+    var planes = [
+        
+        "1. l-16",
+        "2. Jer-2",
+        "3. Yak-9",
+        "4. MiG-3",
+        "5. LaGG-3",
+        "6. La-5FN",
+        "7. il-2",
+        "8. il-2M",
+        "9. il-10",
+        "10. il-4",
+        "11. SB-2M-100",
+        "12. Pe-2 Series 87",
+        "13. Pe-8 (AM-35A)",
+        
+        
+        ]
+        
+        
+    for (b = 0; b < planes.length; b++) {
+        console.log (planes [b])
+    }
+        
+        
+}
+    
+    
+soviet ()
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+function inggris () {
+    
+    console.log ("\n Inggris : \n")
+    
+    
+    var planer = [
+        
+        "1. Hurricane MK IA",
+        "2. Blenheim MK IV",
+        "3. Hurricane MK IIC",
+        "4. Spitfire MK I",
+        "5. Spitfire MK Vc",
+        "6. Spitfire MK IXb",
+        "7. Meteor F. MK I",
+        "8. Typhoon MK Ib",
+        "9. Tempest MK V",
+        "10. Beaufighter TF MK X",
+        "11. Beaufighter MK I",
+        "12. Mosquito FB MK VI",
+        "13. Wellington",
+        "14. Avro Lancaster",
+        "15. Handley Page B Halifax MK III",
+        
+        
+        ]
+        
+        
+    for (c = 0; c < planer.length; c++) {
+        console.log (planer [c])
+    }
+
+
+}
+
+
+inggris ()
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+function nazi_jerman () {
+    
+    console.log ("\n Nazi Jerman \n")
+    
+    
+    var planef = [
+        
+        "1. Bf 109",
+        "2. Fw 190",
+        "3. Me 262",
+        "4. Bf 110",
+        "5. Ju 87 Stuka",
+        "6. Ju 88",
+        "7. He 111",
+        "8. Do 217",
+        "9. Fw 200 Condor",
+        "10. Me 163 Komet",
+        "11. He 162", 
+        "12. Ar 234 Blitz"
+    
+    ]
+    
+    
+    for (k = 0; k < planef.length; k++) {
+        console.log (planef [k])
+    }
+
+}
+    
+nazi_jerman ()
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+function jepang () {
+    
+    console.log ("\n Jepang \n")
+    
+    
+    var planem = [
+        
+        "1. A6M Zero",
+        "2. Ki-43 Hayabusa",
+        "3. Ki-61 Hien",
+        "4. Ki-84 Hayate",
+        "5. N1K-J Shiden Kai",
+        "6. J2M Raiden",
+        "7. D3A Val",
+        "8. B5N Kate",
+        "9. G4M Betty",
+        "10. Ki-21 Sally",
+        "11. D4Y Suisei",
+        "12. A7M Reppu",
+        
+        
+        ]
+        
+        
+    for (e = 0; e < planem.length; e++) {
+        console.log (planem [e])
+    }
+    
+}
+    
+    
+jepang ()
+
+
+console.log ("\n --- Batas --- \n")
---
+++ 1._A._Latihan_Random_Coding/1._Python_dan_JavaScript/Python/1._Pesawat_Tempur_Jaman_Perang_Dunia_Kedua_dalam_Program_Python.py
@@ -0,0 +1,182 @@
+# Pesawat Tempur Jaman Perang Dunia Kedua dalam Program Python
+
+
+print ("\n Pesawat Tempur Jaman Perang Dunia Kedua dalam Program Python \n")
+
+
+def amerika_serikat ():
+    
+    print ("\n Amerika Serikat : \n ")
+    
+    plane = [
+        
+        "1. F4F Wildcat",
+        "2. SBD Dauntless",
+        "3. F4U Corsair",
+        "4. F6F Hellcat",
+        "5. P-51 Mustang",
+        "6. P-38 Lightning",
+        "7. SB2C Helldiver",
+        "8. B-29 Superfortress",
+        "9. B-17 Flying Fortress",
+        "10. A-20 Havoc",
+        "11. B-25 Mitchell",
+        "12. PBY Catalina",
+        
+        
+        ]
+        
+    for a in plane:
+        print (a)
+    
+    
+amerika_serikat ()
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+def soviet ():
+    
+    print ("\n Uni Soviet : \n")
+    
+    planes = [
+        
+        "1. l-16",
+        "2. Jer-2",
+        "3. Yak-9",
+        "4. MiG-3",
+        "5. LaGG-3",
+        "6. La-5FN",
+        "7. il-2",
+        "8. il-2M",
+        "9. il-10",
+        "10. il-4",
+        "11. SB-2M-100",
+        "12. Pe-2 Series 87",
+        "13. Pe-8 (AM-35A)",
+        
+        
+        ]
+        
+        
+    for b in planes:
+        print (b)
+    
+    
+soviet ()
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+def inggris ():
+    
+    print ("\n Inggris : \n")
+    
+    
+    planer = [
+        
+        "1. Hurricane MK IA",
+        "2. Bleinheim MK IV",
+        "3. Hurricane MK IIC",
+        "4. Spitfire MK I",
+        "5. Spitfire MK Vc",
+        "6. Spitfire MK IXb",
+        "7. Meteor F. MK I",
+        "8. Typhoon MK Ib",
+        "9. Tempest MK V",
+        "10. Beaufighter TF MK X",
+        "11. Beaufighter MK I",
+        "12. Mosquito FB MK VI",
+        "13. Wellington",
+        "14. Avro Lancaster",
+        "15. Handley Page B Halifax MK III",
+        
+        
+        ]
+        
+        
+    for c in planer:
+        print (c)
+    
+    
+inggris ()
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+def nazi_jerman ():
+    
+    print ("\n Nazi Jerman \n")
+    
+    
+    planef = [
+        
+        "1. Bf 109",
+        "2. Fw 190",
+        "3. Me 262",
+        "4. Bf 110",
+        "5. Ju 87 Stuka",
+        "6. Ju 88",
+        "7. He 111",
+        "8. Do 217",
+        "9. Fw 200 Condor",
+        "10. Me 163 Komet",
+        "11. He 162", 
+        "12. Ar 234 Blitz"
+    
+    ]
+    
+    
+    for k in planef:
+        print (k)
+
+    
+nazi_jerman ()
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+def jepang ():
+    
+    print ("\n Jepang \n")
+    
+    
+    planem = [
+        
+        "1. A6M Zero",
+        "2. Ki-43 Hayabusa",
+        "3. Ki-61 Hien",
+        "4. Ki-84 Hayate",
+        "5. N1K-J Shiden Kai",
+        "6. J2M Raiden",
+        "7. D3A Val",
+        "8. B5N Kate",
+        "9. G4M Betty",
+        "10. Ki-21 Sally",
+        "11. D4Y Suisei",
+        "12. A7M Reppu",
+        
+        
+        ]
+        
+        
+    for e in planem:
+        print (e)
+    
+    
+jepang ()
+
+
+print ("\n --- Batas --- \n")
