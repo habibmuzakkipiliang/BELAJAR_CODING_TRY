a087315773c8f5194e48caf02c2113dc861ec6fe
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-06-12 21:32:12 +0700

    Add files via upload

---
+++ 1._A._Latihan_Random_Coding/1._Demo_Coding/Demo_Error_Handling/Error_Handling_Demo_JavaScript.js
@@ -0,0 +1,85 @@
+console.log ("\n Error Handling \n")
+
+
+try {
+    var hasil = 10 / A
+    console.log (hasil)
+}
+
+catch (Error) {
+    console.log ("Gagal")
+}
+ 
+finally {
+    console.log ("Selesai")
+}
+
+
+
+console.log ("\n --- batas --- \n")
+
+
+
+
+console.log ("\n Error Handling 2 \n")
+
+
+try {
+    var hasil = 10 / W
+    console.log (hasil)
+}
+
+catch (Error) {
+    console.log ("Gagal")
+}
+
+finally {
+    console.log ("Selesai")
+}
+
+
+console.log ("\n --- batas --- \n")
+
+
+
+
+console.log ("\n Error Handling \n")
+
+
+try {
+    var hasil = 10 + 10
+    console.log (hasil)
+}
+
+catch (Error) {
+    console.log ("Gagal")
+}
+
+finally {
+    console.log ("Selesai")
+}
+
+
+console.log ("\n --- batas --- \n")
+
+
+
+
+console.log ("\n Error Handling 3 \n")
+
+
+try {
+    var hasil = 30 + 30
+    console.log (hasil)
+}
+
+catch (Error) {
+    console.log ("Gagal")
+}
+
+finally {
+    console.log ("Selesai")
+}
+
+
+console.log ("\n --- batas --- \n")
---
+++ 1._A._Latihan_Random_Coding/Profil_Nama_Orang_Remake/Profil_panggilkez_remake.js
@@ -0,0 +1,35 @@
+// Profil Kezia Samantha
+
+
+console.log ("\n Profil Kezia Samantha \n")
+
+
+var nama = "Kezia Samantha"
+var panggil = "Kezi"
+var ig = "@keziiaas"
+var tiktok = "@panggilkez"
+var asal = "Surabaya"
+var etnis = "Tionghoa"
+var alumni = "SMA Petra 1 Surabaya (Alumni) tahun 2026"
+var lahir = "2008"
+var gen = "Z"
+var agama = "Kristen"
+
+
+
+var profil = `
+
+- Nama lengkap   : ${nama}
+- Nama panggilan : ${panggil}
+- Instagram      : ${ig}
+- Tiktok         : ${tiktok}
+- Asal daerah    : ${asal}
+- Alumni         : ${alumni}
+- Tahun lahir    : ${lahir}
+- Generasi       : ${gen}
+- Agama          : ${agama}
+              
+
+`
+
+console.log (profil)
