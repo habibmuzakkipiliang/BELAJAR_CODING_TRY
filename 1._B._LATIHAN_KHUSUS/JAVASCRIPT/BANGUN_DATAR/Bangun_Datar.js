81f6490fbab6c79fc835d6a5542a3be1c28b0d96
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-07-02 05:47:50 +0700

    Add files via upload

---
+++ 1._B._LATIHAN_KHUSUS/JAVASCRIPT/BANGUN_DATAR/Bangun_Datar.js
@@ -0,0 +1,65 @@
+// Rumus Bangun datar
+
+console.log ("\n Rumus Bangun Datar \n")
+
+
+console.log ("\n Luas Bangun Datar \n")
+
+
+console.log ("\n Persegi \n")
+
+
+var luas_persegi = 9 * 9
+
+
+console.log ("Luas persegi = ", luas_persegi)
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Persegi Panjang \n")
+
+var luas_persegi_panjang = 20 * 5
+
+console.log ("Luas persegi panjang = ", luas_persegi_panjang)
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Luas Lingkaran \n")
+
+var luas_lingkaran = 3.14 * 2 * 2
+
+console.log ("Luas lingkaran = ", luas_lingkaran)
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+console.log ("\n Luas Layang-layang \n")
+
+var luas_layang_layang = 10 * 30 / 2
+
+console.log ("Luas layang-layang = ", luas_layang_layang)
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Luas Jajar Genjang \n")
+
+var jajar_genjang = 10 * 5
+
+console.log ("Luas jajar genjang = ", jajar_genjang)
+
+
+console.log ("\n --- Batas --- \n")
