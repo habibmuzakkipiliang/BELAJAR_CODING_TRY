77e53998a7874c3366fa788d9ac217544fe71299
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-07-02 21:16:22 +0700

    Add files via upload

---
+++ 1._B._LATIHAN_KHUSUS/LATIHAN_SEDERHANA/Python/Latihan_Code_Python.py
@@ -0,0 +1,217 @@
+print ("Hello World")
+
+
+print ("\n --- Batas --- \n")
+
+
+teks = "Halo dunia"
+print (teks)
+
+
+angka = 12
+print (angka)
+
+
+desimal = 3.14
+print (desimal)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+print ("\n Tipe Data Pemrograman \n")
+
+nama = "Habib"
+angka = 12
+desimal = 3.14
+cek = True
+char = 'A'
+kosong = None
+
+detail = f"""
+- Nama     : {nama}
+- Angka    : {angka}
+- Desimal  : {desimal}
+- Boolean  : {cek}
+- Char     : {char}   
+- Kosong   : {kosong}
+"""
+
+print (detail)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Fungsi dengan kalkulator \n") 
+
+def tambah (a, b):
+     return a + b
+
+
+def kurang (x, y):
+     return x - y
+
+
+def kali (w, e):
+     return w * e
+
+
+def bagi (z, v):
+     return z / v
+
+
+def pangkat (j, l):
+     return j ** l
+
+
+def modulus (y, g):
+     return y % g
+
+
+hasil_1 = tambah (10, 5)
+hasil_2 = kurang (10, 5)
+hasil_3 = kali (10, 5)
+hasil_4 = bagi (10, 5)
+hasil_5 = pangkat (10, 5)
+hasil_6 = modulus (10, 5)
+
+print (f"""
+- Hasil tambah : {hasil_1}
+- Hasil kurang : {hasil_2}
+- Hasil kali   : {hasil_3}
+- Hasil bagi   : {hasil_4}
+- Hasil pangkat: {hasil_5}
+- Hasil modulus: {hasil_6}
+""")
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Operator Perbandingan \n")
+
+x = 10
+y = 5
+
+detail = f"""
+- Hasil  : {x == y}
+- Hasil  : {x > y}
+- Hasil  : {x < y}
+- Hasil  : {x <= y}
+- Hasil  : {x >= y}
+- Hasil  : {x != y}
+"""
+
+print (detail)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Operator Logika \n")
+
+detail = f"""
+- Hasil  : {x and y}
+- Hasil  : {x or y}
+- Hasil  : {not x}
+"""
+
+print (detail)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Fungsi dengan rumus bangun datar \n")
+
+
+print ("\n Luas Persegi \n")
+
+def persegi (s):
+     return s * s
+
+hasil_k = persegi (5)
+print ("Luas persegi =", hasil_k)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+print ("\n Luas Persegi Panjang \n")
+
+def persegi_panjang (p, l):
+     return p * l
+
+hasil_e = persegi_panjang (5, 10)
+print ("Luas persegi panjang =", hasil_e)
+
+
+
+
+print ("\n --- Batas --- \n")
+
+
+print ("\n Luas Segitiga \n")
+
+def segitiga (a, t):
+     return a * t / 2
+
+hasil_b = segitiga (5, 10)
+print ("Luas segitiga =", hasil_b)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+print ("\n Luas lingkaran \n")
+
+
+def lingkaran (phi, r):
+     return phi * r * 2
+
+hasil_l = lingkaran (3.14, 5)
+print ("Luas lingkaran =", hasil_l)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Luas Layang-Layang \n")
+
+
+def layang_layang (d1, d2):
+     return d1 * d2 / 2
+
+hasil_t = layang_layang (5, 10)
+print ("Luas layang-layang =", hasil_t)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Luas jajar genjang \n")
+
+def jajar_genjang (a, t):
+     return a * t
+
+hasil_f = jajar_genjang(5, 10)
+print ("Luas jajar genjang =", hasil_f)
+
+
+print ("\n --- Batas --- \n")
