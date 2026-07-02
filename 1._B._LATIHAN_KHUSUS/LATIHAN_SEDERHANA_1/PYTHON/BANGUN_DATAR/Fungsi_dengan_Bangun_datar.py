01cb63b366c2501034f792b3ae6408c5bfd94164
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-07-02 16:31:52 +0700

    Add files via upload

---
+++ 1._B._LATIHAN_KHUSUS/LATIHAN_SEDERHANA_1/PYTHON/BANGUN_DATAR/Fungsi_dengan_Bangun_datar.py
@@ -0,0 +1,117 @@
+# Fungsi dengan rumus Bangun datar
+
+print ("\n Fungsi dengan rumus Bangun Datar \n")
+
+
+
+print ("\n Luas Bangun Datar \n")
+
+
+
+print ("\n Luas Persegi \n")
+
+
+def luas_persegi (s):
+    
+    return s * s
+    
+hasil_1 = luas_persegi (9)
+print ("Luas persegi = ", hasil_1)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Luas persegi panjang \n")
+
+
+def luas_persegi_panjang (p, l):
+    
+    return p * l
+    
+hasil_2 = luas_persegi_panjang (20, 5)
+print ("Luas persegi panjang = ", hasil_2)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Luas Segitiga \n")
+
+
+def luas_segitiga (a, t):
+    
+    return a * t 
+    
+hasil_3 = luas_segitiga (10, 10)
+print ("Luas segitiga = ", hasil_3)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Luas Lingkaran \n")
+
+
+def lingkaran (phi, r):
+    
+    return phi * r * r
+    
+hasil_4 = lingkaran (3.14, 2)
+print ("Luas lingkaran = ", hasil_4)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Luas belah ketupat \n")
+
+
+def luas_belah_ketupat (d1, d2):
+    
+    return d1 * d2 / 2
+    
+hasil_5 = luas_belah_ketupat (10, 20)
+print ("Luas belah ketupat =", hasil_5)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Luas layang-layang \n")
+
+
+def luas_layang_layang (d1, d2):
+    
+    return d1 * d2 / 2
+    
+hasil_6 = luas_layang_layang (10, 30)
+print ("Luas belah ketupat =", hasil_6)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Luas Jajar Genjang \n")
+
+def jajar_genjang (a, t):
+    
+    return a * t
+    
+hasil_7 = jajar_genjang (10, 5) 
+print ("Luas jajar genjang =", hasil_7)
+
+
+print ("\n --- Batas --- \n")
