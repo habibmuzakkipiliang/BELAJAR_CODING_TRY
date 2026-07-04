5f6e206fc314e5c16fcdb424aad7ac21a6fbd939
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-07-04 08:33:31 +0700

    Add files via upload

---
+++ 1._A._Latihan_Random_Coding/Coding_Python/Operator_dasar_perbandingan_logika_tambah.py
@@ -0,0 +1,45 @@
+x = int (input ("Masukkan angka x : "))
+y = int (input ("Masukkan angka y : "))
+
+
+print ("\n Operasi Dasar \n")
+
+
+print ("Tambah =", x + y)
+print ("Kurang =", x - y)
+print ("Kali =", x * y)
+print ("Bagi =", x / y)
+print ("Pangkat =", x ** y)
+print ("Modulus =", x % y)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Operasi Perbandingan \n")
+
+print ("Hasil =", x > y)
+print ("Hasil =", x < y)
+print ("Hasil =", x == y)
+print ("Hasil =", x != y)
+print ("Hasil =", x >= y)
+print ("Hasil =", x <= y)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Operasi Logika \n")
+
+
+print ("Hasil =", x > y and x < y)
+print ("Hasil =", x > y or x < y)
+print ("Hasil =", not x)
+print ("Hasil =", not y)
+
+
+print ("\n --- Batas --- \n")
---
+++ 1._A._Latihan_Random_Coding/Profil_Nama_Orang_Remake/Profil_vfeliciwa_remake.py
@@ -0,0 +1,46 @@
+# Profil vfeliciwa
+
+
+print ("\n Profil vfeliciwa \n")
+
+
+nama = "Felicia Kurniawati"
+akrab = "Feli"
+usia = "22 tahun"
+tahun_lahir = "2003"
+alumni = "SMA Katolik Kolese Santo Yusup Malang"
+asal = "Malang"
+agama = "Katolik"
+bisnis = "Pemilik Kafe Cimi Time Duck Malang"
+job = "Konten Kreator"
+hewan = "Bebek Peking"
+bebek = "Cimi, Chipi dan Mbok to"
+gen = "Z"
+etnis = "Tionghoa - Jawa"
+ig = "vfeliciwa"
+tiktok = "vfeliciwa"
+youtube = "vfeliciwa"
+
+
+profil = f"""
+
+- Nama lengkap   : {nama}
+- Nama panggilan : {akrab}
+- Usia           : {usia}
+- Tahun lahir    : {tahun_lahir}
+- Alumni         : {alumni}
+- Asal daerah    : {asal}
+- Agama          : {agama}
+- Bisnis         : {bisnis}
+- Kerja          : {job}
+- Hewan          : {hewan}
+- Bebek          : {bebek}
+- Generasi       : {gen}
+- Etnis          : {etnis}
+- Instagram      : {ig}
+- Tiktok         : {tiktok}
+- Youtube        : {youtube}
+
+"""
+
+print (profil)
